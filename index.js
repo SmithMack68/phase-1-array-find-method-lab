1b0be9b63814f3501551c5a7fad75e5bf7b6bf32
Candice Smith MacKenzie <Mahony8@aol.com>
2021-10-13 21:21:50 -0400

    commit tree 68b2cb405e49062289b6d019e8f7a33d11f68da1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record) {
+    return record.result === "W";
+}
+function isWin(array) {
+    const winner = array.find(isWin);
+    if (winner) {
+        return winner.team;
+    } return winner;
+}
