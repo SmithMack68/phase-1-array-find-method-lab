3c5baac2c6e482335264c78c36c50d47cbcffd62
Candice Smith MacKenzie <Mahony8@aol.com>
2021-12-02 18:25:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 
+
+
+
+
+ 
+function superbowlWin(record){
+    let matchedItem = record.find((element) =>{
+        return element.result === "W"
+    })
+        if(matchedItem){
+            return matchedItem.year
+        }
+         else{
+             return matchedItem
+         }   
+   }
+//    superbowlWin(record, "W");
+
+
+
+
+
+
+
+
+// if(matchedItem = true)
+// }
+// function isWin(array) {
+//     const winner = array.find(isWin);
+//     if (winner) {
+//         return winner.team;
+//     } return winner;
+// }
+
